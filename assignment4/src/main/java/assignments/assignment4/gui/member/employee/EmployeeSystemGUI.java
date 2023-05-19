2cbf409cde821c92a94e5092f91e8597606614aa
Alden <alden.luthfi@ui.ac.id>
2023-05-19 23:39:33 +0700

    More documentations

--- assignment4/src/main/java/assignments/assignment4/gui/member/employee/EmployeeSystemGUI.java
+++ assignment4/src/main/java/assignments/assignment4/gui/member/employee/EmployeeSystemGUI.java
@@ -1,6 +1,6 @@
 package assignments.assignment4.gui.member.employee;
 
-import assignments.assignment3.nota.Nota;
+import assignments.assignment3.nota.Nota;                                       /* Import project files               */
 import assignments.assignment3.nota.NotaManager;
 
 import assignments.assignment3.user.menu.SystemCLI;
@@ -10,21 +10,21 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionListener;
 
-public class EmployeeSystemGUI extends AbstractMemberGUI {
+public class EmployeeSystemGUI extends AbstractMemberGUI {                      /* Page for the launcry staff         */
     public static final String KEY = "EMPLOYEE";
 
-    public EmployeeSystemGUI(SystemCLI systemCLI) {
+    public EmployeeSystemGUI(SystemCLI systemCLI) {                             /* Creates an instance                */
         super(systemCLI);
     }
 
 
     @Override
-    public String getPageName(){
+    public String getPageName(){                                                /* Getters                            */
         return KEY;
     }
 
     @Override
-    protected JButton[] createButtons() {
+    protected JButton[] createButtons() {                                       /* Creates widgets                    */
         return new JButton[]{
             new JButton("It's nyuci time!"),
             new JButton("Display list nota")
@@ -32,14 +32,14 @@ public class EmployeeSystemGUI extends AbstractMemberGUI {
     }
 
     @Override
-    protected ActionListener[] createActionListeners() {
+    protected ActionListener[] createActionListeners() {                        /* Creates listeners                  */
         return new ActionListener[]{
             e -> cuci(),
             e -> displayNota(),
         };
     }
 
-    private void displayNota() {
+    private void displayNota() {                                                /* displays work is and isn't done    */
         String notaListString = "";
 
         for (Nota nota : NotaManager.notaList) if (nota != null){
@@ -59,7 +59,7 @@ public class EmployeeSystemGUI extends AbstractMemberGUI {
         );
     }
 
-    private void cuci() {
+    private void cuci() {                                                       /* displays work thats being done     */
         String notaStatusString = "";
 
         for (Nota nota: NotaManager.notaList) if (nota != null) {
